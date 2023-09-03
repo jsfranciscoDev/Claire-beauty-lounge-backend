bb17622ae3e825fc26e3183fd51259785f249663
jsfranciscoDev <stevenfrancisco.dev@gmail.com>
2023-09-03 11:54:37 +0800

    initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $items = [
+            [
+                'id' => '1',
+                'first_name' => 'admin',
+                'last_name' => 'admin',
+                'email'=>'admin@gmail.com',
+                'password' =>bcrypt('Admin@123'),
+                'role_id' =>1,
+            ],
+        ];
+
+        foreach ($items as $item) {
+            \App\Models\User::updateOrCreate([
+                'id' => $item['id'],
+            ], $item);
+        }
+
+        $roles = [
+            [
+                'id' => '1',
+                'role' => 'admin',
+                'role_description' => 'admin',
+            ],
+            [
+                'id' => '2',
+                'role' => 'staff',
+                'role_description' => 'staff',
+            ],
+            [
+                'id' => '3',
+                'role' => 'user',
+                'role_description' => 'user',
+            ],
+        ];
+
+        // Use the insert method to insert multiple records
+         DB::table('user_roles')->insert($roles);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\ProductController;
+use \App\Http\Controllers\AuthController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//public routes
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+//protected routes
+Route::middleware('auth:sanctum')->group( function () {
+    // Route::get('products', ProductController::class);
+    // Route::get('product', [ProductController::class, 'show'])->name('product');
+    Route::post('/logout',[AuthController::class, 'logout']);
+    Route::get('product', [ProductController::class, 'show'])->name('product');
+});
+
