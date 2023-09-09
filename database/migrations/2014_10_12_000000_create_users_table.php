05cc94a66c90bda2e506c76d422ba2e65e973514
jsfranciscoDev <stevenfrancisco.dev@gmail.com>
2023-09-09 10:06:45 +0800

    fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->smallInteger('active');
+            $table->smallInteger('active')->default(1);
             $table->rememberToken();
             $table->timestamps();
             $table->binary('profile_picture')->nullable(); 
